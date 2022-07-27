82daec8549d794c748493cd3bd38e1888412505e
Chethan Gowda <chethanrgowda38@gmail.com>
2022-07-27 10:04:41 +0530

    Add program

--- 12/Program.cs
+++ 12/Program.cs
@@ -29,7 +29,7 @@ while (i <= n)
 }
 Console.WriteLine();
 
-/*Console.WriteLine();
+Console.WriteLine();
 i = 1;
 while (i <= n)
 {
@@ -37,6 +37,62 @@ while (i <= n)
     Console.Write($"{val} ");
     i++;
 }
-Console.WriteLine();*/
+Console.WriteLine();
+
+Console.WriteLine();
+i = 4;
+int value = 1;
+int f1 = 3;
+int f2 = 3;
+int f3 = 5;
+int temp;
+
+Console.Write($"{value} ");//1
+value = value + f1;
+Console.Write($"{value} ");//4
+value = value + f2;
+Console.Write($"{value} ");//7
+value = value + f3;
+Console.Write($"{value} ");//12
+
+while ( i <= n)
+{
+    temp = f3;//5
+    value = value+ f1+ f2+ f3;
+    Console.Write($"{value} ");//23
+    f3 = f1 + f2 + f3;//11
+    f1 = f2;//3
+    f2 = temp;//5
+    i++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+i = 1;
+while (i <= n)
+{
+    if (i % 4 != 0)
+    {
+        val = Math.Pow(i, 2);
+        Console.Write($"{val} ");
+    }
+    i++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+i = 1;
+val = 1;
+while (i <= n)
+{
+    
+    if (i % 3 != 0)
+    {
+        Console.Write($"{val} ");
+        val = val + i * 4;
+    }
+    i++;
+}
+Console.WriteLine();
 
 
