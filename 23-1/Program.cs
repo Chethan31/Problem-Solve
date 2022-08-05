bffa774aec09d0d6452fef0ce1125d7529e46789
Chethan Gowda <chethanrgowda38@gmail.com>
2022-08-05 14:10:55 +0530

    Add

---
+++ 23-1/Program.cs
@@ -0,0 +1,54 @@
+﻿int count = 0;
+int N;
+int value=800;
+int sum = 0;
+int temp;
+int flag;
+List<int> list1 = new List<int>();
+List<int> list2 = new List<int>();
+
+Console.WriteLine("Enter N Value:");
+N=int.Parse(Console.ReadLine());
+Console.WriteLine("The series is:");
+for(int i=0; value<N; i++)
+{   
+    value = value + (2*i);
+    if (value < N)
+    {
+        list1.Add(value);
+        temp = value;
+        sum = 0;
+        flag = 0;
+        while (temp != 0)
+        {
+            sum = sum + temp % 10;
+            temp = temp / 10;
+        }
+        list2.Add(sum);
+        while (sum != 0)
+        {
+            if (sum % 10 == 1)
+            {  
+                if(flag==0)
+                    count++;
+                flag= 1;
+            }
+            sum = sum / 10;  
+        } 
+    }
+}
+//int[] arr = list.ToArray();
+
+foreach(int i in list1)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+foreach (int i in list2)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+Console.WriteLine($"The count of the sum numbers which have the digit 1:{count}");
+
+
